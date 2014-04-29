f5dca1c383ddbe79b8a84b97627ead17f6c43bc5
BigBlueHat <byoung@bigbluehat.com>
2014-04-29 11:42:54 -0400

    added `date` to search fields for range counting

--- couchapp/indexes/mail/index.js
+++ couchapp/indexes/mail/index.js
@@ -5,5 +5,7 @@ function (doc) {
     index('from', doc.from, {store: true, facet: true});
     index('subject', doc.subject.replace('Re: ', '').replace('Fwd: ', ''),
       {store: true, facet: true});
+    // generate a number based on year + month for range counting
+    index('date', (doc.date[0]*100)+doc.date[1], {store: true, facet: true});
   }
 }
