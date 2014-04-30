482a2b6632f29018b9b6e8ff20e7d284ae555743
BigBlueHat <byoung@bigbluehat.com>
2014-04-30 16:24:01 -0400

    added full_date index field for display

--- couchapp/indexes/mail/index.js
+++ couchapp/indexes/mail/index.js
@@ -7,5 +7,7 @@ function (doc) {
       {store: true, facet: true});
     // generate a number based on year + month for range counting
     index('date', (doc.date[0]*100)+doc.date[1], {store: true, facet: true});
+    index('full_date', doc.date[0]+'-'+doc.date[1]+'-'+doc.date[2]+'T'
+        +doc.date[3]+':'+doc.date[4]+':'+doc.date[5], {store:true});
   }
 }
