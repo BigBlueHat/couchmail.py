113e304e02fd4917a14e702163b0bec503e6d76f
BigBlueHat <byoung@bigbluehat.com>
2015-01-19 12:27:08 -0500

    Clean up the mess I have been making

--- couchapp/shows/message.js
+++
@@ -1,8 +0,0 @@
-function (doc, req) {
-  var mustache = require('lib/mustache');
-  if (doc) {
-    return mustache.to_html(this.templates.layout, {message: doc}, this.templates);
-  } else {
-    // 404
-  }
-}
